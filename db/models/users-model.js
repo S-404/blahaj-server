5b610997dc36d805d2c48b590725aaa158ac097f
S-404 <sudarev.andrey.i@gmail.com>
2022-04-22 23:03:06 +0300

    add user-model

---
+++ db/models/users-model.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+
+module.exports = function (sequelize) {
+
+    return sequelize.define('users', {
+        id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        isActivated: {
+            type: Sequelize.BOOLEAN
+        },
+    }, {
+        timestamps: false
+    })
+}
