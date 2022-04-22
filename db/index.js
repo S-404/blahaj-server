64a6665e5652f21ad21fee6eff9cd1ace7090f62
S-404 <sudarev.andrey.i@gmail.com>
2022-04-22 23:07:32 +0300

    add db sequelize/mssql settings

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE_NAME,
+    process.env.DB_USER,
+    process.env.DB_USER_PASSWORD,
+    {
+        host: process.env.DB_SERVER_NAME,
+        dialect: 'mssql'
+    },
+)
+
+const TokensModel = require('./models/tokens-model')(sequelize)
+const UsersModel = require('./models/users-model')(sequelize)
+
+module.exports = {
+    sequelize: sequelize,
+    TokensModel: TokensModel,
+    UsersModel: UsersModel,
+}
