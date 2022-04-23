139de56060c1af52193535b665623b60db7b2fad
S-404 <sudarev.andrey.i@gmail.com>
2022-04-23 11:13:22 +0300

    add middlewares to index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 require('dotenv').config()
 
 const authRouter = require('./routes/auth-routes')
+const apiErrorMiddleware = require("./middlewares/error-middleware")
+const authMiddleware = require("./middlewares/auth-middleware")
 
 const PORT = process.env.PORT
 const app = express()
@@ -14,7 +16,9 @@ app.use(cors())
 // app.use(cors({credentials: true, origin: process.env.CLIENT_URL}))
 
 app.use('/auth', authRouter)
+app.use(authMiddleware)
 
+app.use(apiErrorMiddleware)
 app.listen(PORT, function () {
     console.log(`Server is running on port ${PORT}`)
 })
